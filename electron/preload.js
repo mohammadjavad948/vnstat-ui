6c8d7a5238b6a233577ddc6ce5fabb5156c3517c
mohammadjavad <mjkmo123@gmail.com>
2021-07-19 11:25:35 +0430

    preload

--- electron/preload.js
+++ electron/preload.js
@@ -1,1 +1,25 @@
-console.log('preload')
+const {
+    contextBridge,
+    ipcRenderer
+} = require("electron");
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld(
+    "api", {
+        send: (channel, data) => {
+            // whitelist channels
+            let validChannels = ['close', 'minimize', 'maximize', 'unmaximize'];
+            if (validChannels.includes(channel)) {
+                ipcRenderer.send(channel, data);
+            }
+        },
+        receive: (channel, func) => {
+            let validChannels = ['maximized', 'unmaximized'];
+            if (validChannels.includes(channel)) {
+                // Deliberately strip event as it includes `sender`
+                ipcRenderer.on(channel, (event, ...args) => func(...args));
+            }
+        }
+    }
+);
