af2a09b08666347351d2b83c6f1ae924bf381a85
mohammadjavad <mjkmo123@gmail.com>
2021-07-20 11:17:38 +0430

    oka

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import YouAreNotRunningOnElectron from "./component/YouAreNotRunningOnElectron";
 import {useIsElectron} from "./hooks/electron";
+import Container from "./Container";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
   return (
     <div>
       {!isElectron && <YouAreNotRunningOnElectron />}
+      {isElectron && <Container />}
     </div>
   );
 }
