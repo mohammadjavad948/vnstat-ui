2180e9589e557c41413343946c3d7ec55e1c12f8
mohammadjavad <mjkmo123@gmail.com>
2021-07-20 08:36:06 +0430

    full interface

--- react-ui/src/type/response.ts
+++ react-ui/src/type/response.ts
@@ -19,13 +19,58 @@ export interface Interfaces {
 
 export interface Traffic{
     day: DayTraffic[]
+    fiveminute: FiveMinuteTraffic[]
+    hour: HourTraffic[]
+    month: MonthTraffic[]
+    top: TopTraffic[]
+    year: YearTraffic[]
     total: {
         rx: number
         tx: number
     }
 }
 
+export interface FiveMinuteTraffic{
+    rx: number
+    tx: number
+    id: number
+    date: VnDate
+    time: VnTime
+}
+
+export interface HourTraffic{
+    rx: number
+    tx: number
+    id: number
+    date: VnDate
+    time: VnTime
+}
+
 export interface DayTraffic{
+    rx: number
+    tx: number
+    id: number
+    date: VnDate
+}
+
+
+export interface MonthTraffic{
+    rx: number
+    tx: number
+    id: number
+    date: VnDate
+}
+
+
+export interface TopTraffic{
+    rx: number
+    tx: number
+    id: number
+    date: VnDate
+}
+
+
+export interface YearTraffic{
     rx: number
     tx: number
     id: number
