ef6b36fb2e551d73ed4671c75b9abd54064b76f1
mohammadjavad <mjkmo123@gmail.com>
2021-07-20 08:22:11 +0430

    hmmm

--- react-ui/src/type/response.ts
+++ react-ui/src/type/response.ts
@@ -1,3 +1,40 @@
 export interface VnstatI{
+    vnstatversion: string
+    jsonversion: string
+    interfaces: Interfaces[]
+}
 
+export interface Interfaces {
+    alias: string
+    name: string
+    created: {
+        date: VnDate
+    }
+    updated: {
+        date: VnDate,
+        time: VnTime
+    }
+    traffic: Traffic
+}
+
+export interface Traffic{
+    day: DayTraffic[]
+}
+
+export interface DayTraffic{
+    rx: number
+    tx: number
+    id: number
+    date: VnDate
+}
+
+export interface VnDate{
+    day: number
+    month: number
+    year: number
+}
+
+export interface VnTime{
+    hour: number
+    minute: number
 }
