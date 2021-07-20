3879efeb62de3d207c55dfaeaa073d3609eac5ab
mohammadjavad <mjkmo123@gmail.com>
2021-07-20 11:24:23 +0430

    init

--- react-ui/src/Container.tsx
+++ react-ui/src/Container.tsx
@@ -1,5 +1,24 @@
+import {useEffect} from "react";
+import {getInterfaces} from "./api/vnstat";
+import {useInterfaceStore} from "./store/interfaceStore";
 
 export default function Container(){
+
+    const {setInterfaces, setSelected, selected} = useInterfaceStore();
+
+    async function init(){
+
+        const interfaces = await getInterfaces();
+
+        const inter = interfaces.split(' ').splice(0, 2);
+        inter.splice(inter.length - 1, 1);
+
+        setInterfaces(inter);
+    }
+
+    useEffect(() => {
+
+    }, []);
 
     return (
         <div>
