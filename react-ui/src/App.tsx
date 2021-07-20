5679cbba6c676cde206dea29300e9acf502644f5
mohammadjavad <mjkmo123@gmail.com>
2021-07-20 11:11:31 +0430

    remove css

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import './App.css';
 import YouAreNotRunningOnElectron from "./component/YouAreNotRunningOnElectron";
 import {useIsElectron} from "./hooks/electron";
 
