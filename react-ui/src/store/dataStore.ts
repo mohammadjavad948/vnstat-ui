01d87dba80df9d7733dbab3bcddc96ad30c4879d
mohammadjavad <mjkmo123@gmail.com>
2021-07-21 20:58:25 +0430

    data store

---
+++ react-ui/src/store/dataStore.ts
@@ -0,0 +1,17 @@
+import create from "zustand";
+import {VnstatI} from "../type/response";
+
+interface DataI{
+    data: VnstatI,
+    setData: (data: VnstatI) => void
+}
+
+// @ts-ignore
+export const useDataStore = create<DataI>((set) => {
+    return {
+        data: {},
+        setData: (data) => {
+            set({data});
+        }
+    }
+})
