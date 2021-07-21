652b19fcb0f03ebcdd7a04d2ba2313fd43f7e51f
mohammadjavad <mjkmo123@gmail.com>
2021-07-21 21:29:03 +0430

    more icon

--- react-ui/src/component/sidebar/Sidebar.tsx
+++ react-ui/src/component/sidebar/Sidebar.tsx
@@ -1,18 +1,35 @@
 import style from './sidebar.module.css';
 import PrettyByte from 'pretty-bytes';
 import {useDataStore} from "../../store/dataStore";
+import {IconButton} from "@material-ui/core";
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import FilterListIcon from '@material-ui/icons/FilterList';
+import {useInterfaceStore} from "../../store/interfaceStore";
+import ImportExportIcon from '@material-ui/icons/ImportExport';
+import WifiIcon from '@material-ui/icons/Wifi';
 
 export default function Sidebar(){
 
     const {data} = useDataStore();
+    const {selected} = useInterfaceStore();
 
     return (
         <div className={style.sidebar}>
             <div className={style.items}>
-
+                <IconButton>
+                    <DashboardIcon />
+                </IconButton>
+                <IconButton>
+                    <FilterListIcon />
+                </IconButton>
             </div>
             <div className={style.items}>
-
+                <IconButton>
+                    <ImportExportIcon />
+                </IconButton>
+                <IconButton>
+                    <WifiIcon />
+                </IconButton>
             </div>
         </div>
     )
