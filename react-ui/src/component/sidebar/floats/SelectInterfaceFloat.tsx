03c8685c78b57f193b86a268d0a36c87da978c47
mohammadjavad <mjkmo123@gmail.com>
2021-07-22 16:13:38 +0430

    basic float

--- react-ui/src/component/sidebar/floats/SelectInterfaceFloat.tsx
+++ react-ui/src/component/sidebar/floats/SelectInterfaceFloat.tsx
@@ -1,10 +1,30 @@
 import style from './select.module.css';
+import Float from "../../float/Float";
 
-export default function SelectInterfaceFloat(){
+
+interface SelectI{
+    data: any
+    hideFn: any
+}
+
+export default function SelectInterfaceFloat(props: SelectI){
 
     return (
         <div>
-
+            {
+                props.data.show && (
+                    <Float
+                        top={props.data.y}
+                        left={props.data.x}
+                        side={'right'}
+                        hideFn={props.hideFn}
+                    >
+                        <div className={style.title}>
+                            Select Interface
+                        </div>
+                    </Float>
+                )
+            }
         </div>
     )
 }
