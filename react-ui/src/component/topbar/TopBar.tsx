ae76f3f210cc4f6f3ee9265fc06267261c8cbd17
mohammadjavad <mjkmo123@gmail.com>
2021-07-22 16:37:16 +0430

    icons

--- react-ui/src/component/topbar/TopBar.tsx
+++ react-ui/src/component/topbar/TopBar.tsx
@@ -1,10 +1,26 @@
 import style from './topbar.module.css';
+import CloseIcon from '@material-ui/icons/Close';
+import Crop169Icon from '@material-ui/icons/Crop169';
+import MinimizeIcon from '@material-ui/icons/Minimize';
 
 export default function TopBar(){
 
     return (
         <div className={style.container}>
+            <div>
 
+            </div>
+            <div className={style.icons}>
+                <div>
+                    <MinimizeIcon />
+                </div>
+                <div>
+                    <Crop169Icon />
+                </div>
+                <div>
+                    <CloseIcon />
+                </div>
+            </div>
         </div>
     )
 }
