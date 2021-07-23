79959340ada4a591f1fdfc80a1d624bd557cacca
mohammadjavad <mjkmo123@gmail.com>
2021-07-23 11:54:15 +0430

    bug fix

--- react-ui/src/Panel.tsx
+++ react-ui/src/Panel.tsx
@@ -1,10 +1,10 @@
-import style from './container.module.css';
+import style from './panel.module.css';
 
 export default function Panel(){
 
     return (
         <div className={style.container}>
-
+            hmmmmmmmmmmmmmmm
         </div>
     )
 }
