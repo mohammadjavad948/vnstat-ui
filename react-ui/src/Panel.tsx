9dc07c1de8b028b709ee36bc5e6adc995da706bc
mohammadjavad <mjkmo123@gmail.com>
2021-07-23 11:57:46 +0430

    use

--- react-ui/src/Panel.tsx
+++ react-ui/src/Panel.tsx
@@ -1,10 +1,11 @@
 import style from './panel.module.css';
+import DailyUsageChart from "./component/charts/DailyUsageChart";
 
 export default function Panel(){
 
     return (
         <div className={style.container}>
-            hmmmmmmmmmmmmmmm
+            <DailyUsageChart />
         </div>
     )
 }
