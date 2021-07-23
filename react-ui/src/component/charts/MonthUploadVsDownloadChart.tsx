11fb374e4cb7d2480da3992951c65c037719f8cc
mohammadjavad <mjkmo123@gmail.com>
2021-07-23 12:24:52 +0430

    effect

--- react-ui/src/component/charts/MonthUploadVsDownloadChart.tsx
+++ react-ui/src/component/charts/MonthUploadVsDownloadChart.tsx
@@ -1,6 +1,13 @@
 import {Card, CardContent} from "@material-ui/core";
+import {useEffect, useState} from "react";
 
 export default function MonthUploadVsDownloadChart(){
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+
+    }, []);
 
     return (
         <div className="col-lg-6 col-md-12 col-sm-12">
