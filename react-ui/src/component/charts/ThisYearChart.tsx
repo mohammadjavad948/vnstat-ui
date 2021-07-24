2a2405675889fe5de2edd85b448d00ffaa75ab85
mohammadjavad <mjkmo123@gmail.com>
2021-07-24 14:23:23 +0430

    year

---
+++ react-ui/src/component/charts/ThisYearChart.tsx
@@ -0,0 +1,45 @@
+import {Card, CardContent} from "@material-ui/core";
+import {useEffect, useState} from "react";
+import {useDataStore} from "../../store/dataStore";
+import {Doughnut} from "react-chartjs-2";
+import PrettyByte from "pretty-bytes";
+
+export default function ThisYearChart(){
+
+    const [data, setData] = useState({});
+
+    const {data: rawData} = useDataStore();
+
+    useEffect(() => {
+        const month = rawData.interfaces[0].traffic.month;
+
+        const month12 = month.slice(Math.max(month.length - 12, 1));
+
+        setData({
+            labels: month12.map((data) => {
+                const sum = data.rx + data.tx;
+                const month = new Date().setFullYear(data.date.year, data.date.month, 0).toLocaleString();
+                return `${PrettyByte(sum || 0)} ${month}`
+            }),
+            datasets: [
+                {
+                    data: month12.map((data) => {
+                        return data.rx + data.tx;
+                    }),
+                    backgroundColor: 'white',
+                },
+            ],
+        });
+    }, [rawData]);
+
+    return (
+        <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
+            <Card>
+                <CardContent>
+                    <h3>This Year</h3>
+                    <Doughnut data={data} />
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
