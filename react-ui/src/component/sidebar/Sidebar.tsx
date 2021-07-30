3e6b075f39382abc68f1c61000d46bee3ac14f7a
mohammadjavad <mjkmo123@gmail.com>
2021-07-30 10:57:23 +0430

    bug fix

--- react-ui/src/component/sidebar/Sidebar.tsx
+++ react-ui/src/component/sidebar/Sidebar.tsx
@@ -1,7 +1,6 @@
 import style from './sidebar.module.css';
 import {IconButton} from "@material-ui/core";
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import FilterListIcon from '@material-ui/icons/FilterList';
 import ImportExportIcon from '@material-ui/icons/ImportExport';
 import WifiIcon from '@material-ui/icons/Wifi';
 import useFloat from "../../hooks/float";
