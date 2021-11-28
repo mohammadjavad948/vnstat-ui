a870acc1e8af612514bdbfa810af4797938d4eb1
mohammadjavad <mjkmo123@gmail.com>
2021-11-28 15:46:20 +0330

    bindgen

--- rust-processor/src/lib.rs
+++ rust-processor/src/lib.rs
@@ -2,10 +2,14 @@ use std::process::Command;
 
 fn main(){
     let out = Command::new("vnstat")
-        .arg("--json")
-        .arg("-d")
         .arg("-i")
         .arg("wlp2s0")
-        .spawn()
+        .arg("-d")
+        .arg("--limit")
+        .arg("0")
+        .arg("--json")
+        .output()
         .expect("err");
+
+    println!("{}", String::from_utf8_lossy(&out.stdout));
 }
