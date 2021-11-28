3c5c72e55abbb93a1bde29709c8f165868734721
mohammadjavad <mjkmo123@gmail.com>
2021-11-28 15:37:48 +0330

    command

--- rust-processor/src/lib.rs
+++ rust-processor/src/lib.rs
@@ -1,5 +1,11 @@
-
+use std::process::Command;
 
 fn main(){
-
+    let out = Command::new("vnstat")
+        .arg("--json")
+        .arg("-d")
+        .arg("-i")
+        .arg("wlp2s0")
+        .spawn()
+        .expect("err");
 }
