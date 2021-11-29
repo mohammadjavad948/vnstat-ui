b5fc0d4b9480c2c29f3c3a855c55e9c063046d3c
mohammadjavad <mjkmo123@gmail.com>
2021-11-29 09:27:54 +0330

    bindgen

--- rust-processor/src/lib.rs
+++ rust-processor/src/lib.rs
@@ -9,7 +9,7 @@ use serde_json::Result;
 #[wasm_bindgen]
 pub fn days_chart(data: &str) -> String {
 
-    let converted: Result<DaysInterface> = serde_json::from_str(data);
+    let converted: DaysInterface = serde_json::from_str(data).expect("hmmm");
 
     return data.to_string();
 }
