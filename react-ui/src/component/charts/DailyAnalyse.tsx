e469d32ec27a0b12ed2af032bb1002b89b112803
mohammadjavad <mjkmo123@gmail.com>
2021-11-30 17:34:27 +0330

    hmm

---
+++ react-ui/src/component/charts/DailyAnalyse.tsx
@@ -0,0 +1,14 @@
+import {Card, CardContent} from "@material-ui/core";
+
+
+export default function DailyAnalyse(){
+
+    return (
+        <div className="col-lg-4 col-md-6 col-sm-12 mt-2">
+            <Card>
+                <CardContent>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
