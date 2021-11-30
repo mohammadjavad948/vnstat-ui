88a08e97de67cd1f352a9c1b7859e24527f42a7d
mohammadjavad <mjkmo123@gmail.com>
2021-11-30 17:36:06 +0330

    hmm

--- react-ui/src/component/charts/DailyAnalyse.tsx
+++ react-ui/src/component/charts/DailyAnalyse.tsx
@@ -7,6 +7,7 @@ export default function DailyAnalyse(){
         <div className="col-lg-8 col-md-12 col-sm-12 mt-2">
             <Card>
                 <CardContent>
+                    <h3>Daily Usage Analyse (powered by rust)</h3>
                 </CardContent>
             </Card>
         </div>
