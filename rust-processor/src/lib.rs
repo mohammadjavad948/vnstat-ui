616bf78e2991ddd27da3fd23978f6be87a7b5833
mohammadjavad <mjkmo123@gmail.com>
2021-11-30 17:15:34 +0330

    fix

--- rust-processor/src/lib.rs
+++ rust-processor/src/lib.rs
@@ -6,22 +6,6 @@ use structure::DaysInterface;
 use structure::DaysReturn;
 
 use wasm_bindgen::prelude::*;
-use serde_json::Result;
-
-macro_rules! console_log {
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-macro_rules! console_warn {
-    ($($t:tt)*) => (warn(&format_args!($($t)*).to_string()))
-}
-
-#[wasm_bindgen]
-extern {
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-    #[wasm_bindgen(js_namespace = console)]
-    fn warn(s: &str);
-}
 
 #[wasm_bindgen]
 pub fn days_chart(data: &str) -> String {
